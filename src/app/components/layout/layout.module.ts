073b696cdf9c0d0cd67f941793d1c2c0059821f3
Hai <cdh271294@gmail.com>
2021-04-01 23:59:30 +0700

    undate code

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponentModule } from '../header/header-component.module';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @NgModule({
   declarations: [MainLayoutComponent, AuthLayoutComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponentModule],
 
   exports: [MainLayoutComponent, AuthLayoutComponent],
 })
