803b76ccf99636e16c85edc9b88a1c2bad44be31
Bao <hgiabao1998@gmail.com>
2021-04-14 10:19:13 +0700

    update detail placeholder data

--- src/app/pages/main/contract/detail/detail.component.ts
+++ src/app/pages/main/contract/detail/detail.component.ts
@@ -11,37 +11,61 @@ export class DetailComponent implements OnInit {
     heigth: '',
     cssClass: 'base-card',
     title: {
-      text: 'Chi tiết nhân viên bán hàng - #23535',
+      text: 'Chi tiết hợp đồng -#DV865926',
       status: '',
     },
     list: [
       {
-        label: 'text',
-        value: '123456',
+        label: 'Mã hợp đồng',
+        value: 'DV29648265',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Hình thức thanh toán',
+        value: 'Viễn thông',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Ngày tạo',
+        value: '10/12/2019-12:00:00',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Sản phẩm',
+        value: 'Bảo Tâm An',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Ngày hiệu lực',
+        value: '12/12/2019',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Phí bảo hiểm',
+        value: '14.000đ',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Gói sản phẩm',
+        value: 'Cơ bản',
+      },
+      {
+        label: 'Ngày hêt hạn',
+        value: '12/12/2020',
+      },
+      {
+        label: 'Hoa hồng kênh',
+        value: '1.400đ',
+      },
+      {
+        label: 'Nhà cung cấp',
+        value: 'Bảo hiểm Bảo Long',
+      },
+      {
+        label: 'Chu kỳ',
+        value: 'Tuần',
+      },
+      {
+        label: 'Hoa hồng NVBH',
+        value: '1.000đ',
+      },
+      {
+        label: 'Trang thái',
+        value: 'Hoạt động',
       },
     ],
   };
@@ -52,37 +76,25 @@ export class DetailComponent implements OnInit {
     heigth: '',
     cssClass: 'base-card',
     title: {
-      text: 'Chi tiết nhân viên bán hàng - #23535',
+      text: 'Người bán',
       status: '',
     },
     list: [
       {
-        label: 'text',
-        value: '123456',
+        label: 'Mã nhân viên',
+        value: '48265',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Số điện thoại',
+        value: '0987654321',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Họ tên',
+        value: 'Nguyễn Thị An',
       },
       {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
+        label: 'Trạng thái',
+        value: 'Hoạt động',
       },
     ],
   };
@@ -92,37 +104,25 @@ export class DetailComponent implements OnInit {
     heigth: '',
     cssClass: 'base-card',
     title: {
-      text: 'Chi tiết nhân viên bán hàng - #23535',
+      text: 'Người được bảo hiểm',
       status: '',
     },
     list: [
       {
-        label: 'text',
-        value: '123456',
+        label: 'Họ và tên',
+        value: 'Trần Thị B',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Ngày sinh',
+        value: '12/12/1990',
       },
       {
-        label: 'text',
-        value: '123456',
+        label: 'Giới tính',
+        value: 'Nữ',
       },
       {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
-      },
-      {
-        label: 'text',
-        value: '123456',
+        label: 'CMT/CCCD',
+        value: '98562875676',
       },
     ],
   };
