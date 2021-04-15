14e38b25f49dece8ee804fc605d0ba2dfdb7bf72
Hai <cdh271294@gmail.com>
2021-04-15 16:24:50 +0700

    + header login

--- src/app/components/header/header-component.module.ts
+++ src/app/components/header/header-component.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderInfoComponent } from './header-info/header-info.component';
 import { HeaderLogoComponent } from './header-logo/header-logo.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [HeaderLogoComponent, HeaderInfoComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,MatMenuModule,MatIconModule],
 
   exports: [HeaderLogoComponent, HeaderInfoComponent],
 })
